4e2cb345359bec04a6f739115b66e7b619a5e85e
dkoller <dominik.koller@myworld.com>
2022-04-12 10:02:32 +0200

    stuff updated
    target android 12, androidx

--- app/src/main/java/com/sdsmdg/harjot/vectormasterdemo/MainActivity.java
+++ app/src/main/java/com/sdsmdg/harjot/vectormasterdemo/MainActivity.java
@@ -2,45 +2,41 @@ package com.sdsmdg.harjot.vectormasterdemo;
 
 import android.graphics.Color;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
-
+import androidx.appcompat.app.AppCompatActivity;
 import com.sdsmdg.harjot.vectormaster.VectorMasterView;
-import com.sdsmdg.harjot.vectormaster.models.ClipPathModel;
 import com.sdsmdg.harjot.vectormaster.models.GroupModel;
 import com.sdsmdg.harjot.vectormaster.models.PathModel;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     Typeface typeface;
     TextView text;
-
+    
     VectorMasterView lightningView, hourglassView, searchBackView, rainView;
-
+    
     float trimStart = 0;
     float trimEnd = 0;
     int countLightning = 60;
-
+    
     float translationY = 0;
     float rotation = 0;
     int state = 0;
-
+    
     float stemTrimStart = 0;
     float stemTrimEnd = 0.185f;
-
+    
     float circleTrimEnd = 1;
-
+    
     float arrowHeadTopTrimEnd = 0;
     float arrowHeadBottomTrimEnd = 0;
-
+    
     int searchBackState = 0;
     int count = 60;
-
+    
     float rainTrimStart_C = 0;
     float rainTrimEnd_C = 0;
     int rainCount_C = 60;
@@ -50,30 +46,30 @@ public class MainActivity extends AppCompatActivity {
     float rainTrimStart_R = 0;
     float rainTrimEnd_R = 0;
     int rainCount_R = 50;
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
         typeface = Typeface.createFromAsset(getAssets(), "fonts/adequate.ttf");
         text = (TextView) findViewById(R.id.text);
         text.setTypeface(typeface);
-
+        
         lightningView = (VectorMasterView) findViewById(R.id.vector_master);
         animateLightning();
-
+        
         hourglassView = (VectorMasterView) findViewById(R.id.vector_master_1);
         animateHourglass();
-
+        
         searchBackView = (VectorMasterView) findViewById(R.id.vector_master_2);
         animateSearchBack();
-
+        
         rainView = (VectorMasterView) findViewById(R.id.vector_master_3);
         animateRain();
-
+        
     }
-
+    
     void animateLightning() {
         PathModel cloudModel = lightningView.getPathModelByName("cloud");
         cloudModel.setStrokeColor(Color.parseColor("#5D5D5D"));
@@ -81,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
         lightningModel.setStrokeColor(Color.parseColor("#FFD700"));
         lightningModel.setTrimPathEnd(0.0f);
         lightningModel.setTrimPathStart(0.0f);
-
+        
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
@@ -99,26 +95,21 @@ public class MainActivity extends AppCompatActivity {
                     }
                     lightningModel.setTrimPathEnd(trimEnd);
                     lightningModel.setTrimPathStart(trimStart);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            lightningView.update();
-                        }
-                    });
+                    runOnUiThread(() -> lightningView.update());
                 }
             }
         }, 500, 1000 / 60);
     }
-
+    
     void animateHourglass() {
         final GroupModel frame = hourglassView.getGroupModelByName("hourglass_frame");
         final GroupModel fillOutlines = hourglassView.getGroupModelByName("fill_outlines");
         final GroupModel fillOutlinesPivot = hourglassView.getGroupModelByName("fill_outlines_pivot");
         final GroupModel group_fill_path = hourglassView.getGroupModelByName("group_fill_path");
-
+        
         state = 0;
         translationY = -24;
-
+        
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
@@ -161,15 +152,15 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         }, 500, 1000 / 60);
-
+        
     }
-
+    
     void animateSearchBack() {
         final PathModel searchCircle = searchBackView.getPathModelByName("search_circle");
         final PathModel stem = searchBackView.getPathModelByName("stem");
         final PathModel arrowUp = searchBackView.getPathModelByName("arrow_head_top");
         final PathModel arrowDown = searchBackView.getPathModelByName("arrow_head_bottom");
-
+        
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
@@ -197,30 +188,25 @@ public class MainActivity extends AppCompatActivity {
                             count = 0;
                         }
                     }
-
+                    
                     searchCircle.setTrimPathEnd(circleTrimEnd);
                     stem.setTrimPathEnd(stemTrimEnd);
                     stem.setTrimPathStart(stemTrimStart);
                     arrowUp.setTrimPathEnd(arrowHeadTopTrimEnd);
                     arrowDown.setTrimPathEnd(arrowHeadBottomTrimEnd);
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            searchBackView.update();
-                        }
-                    });
+                    
+                    runOnUiThread(() -> searchBackView.update());
                 }
-
+                
             }
         }, 1000, 1000 / 60);
     }
-
+    
     void animateRain() {
         final PathModel rainLeft = rainView.getPathModelByName("rain_left");
         final PathModel rainCenter = rainView.getPathModelByName("rain_center");
         final PathModel rainRight = rainView.getPathModelByName("rain_right");
-
+        
         Timer timer = new Timer();
         timer.scheduleAtFixedRate(new TimerTask() {
             @Override
@@ -261,26 +247,21 @@ public class MainActivity extends AppCompatActivity {
                         rainCount_R = 0;
                     }
                 }
-
+                
                 rainCenter.setTrimPathEnd(rainTrimEnd_C);
                 rainCenter.setTrimPathStart(rainTrimStart_C);
-
+                
                 rainLeft.setTrimPathEnd(rainTrimEnd_L);
                 rainLeft.setTrimPathStart(rainTrimStart_L);
-
+                
                 rainRight.setTrimPathEnd(rainTrimEnd_R);
                 rainRight.setTrimPathStart(rainTrimStart_R);
-
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        rainView.update();
-                    }
-                });
-
+                
+                runOnUiThread(() -> rainView.update());
+                
             }
         }, 500, 1000 / 60);
-
+        
     }
-
+    
 }
